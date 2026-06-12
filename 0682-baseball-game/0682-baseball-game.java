643c4486355cd1240c1335b4a9616e79950faf2a
Sukrut Bidwai <s.bidwai2000@gmail.com>
2026-06-12 13:55:25 -0400

    Time: 3 ms (81.63%), Space: 43.6 MB (46.23%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        
+        Stack<Integer> st = new Stack<>();
+        int score = 0;
+
+        for(String opr : operations){
+            
+            if(opr.equals("+")){
+                int n1 = st.pop();
+                int sum = n1 + st.peek();
+
+                st.push(n1);
+                st.push(sum);
+            }
+            else if(opr.equals("C")){
+                st.pop();
+            }
+            else if(opr.equals("D")){
+                st.push(2 * st.peek());
+            }
+            else{
+                st.push(Integer.parseInt(opr));
+            }
+        }
+
+        while(!st.isEmpty()){
+            score += st.pop();
+        }
+
+        return score;
+    }
+}
