1cfc8cce7a6072a8d4ce2a806f187aa6a5337c51
Sukrut Bidwai <s.bidwai2000@gmail.com>
2026-07-28 20:22:56 +0530

    Time: 0 ms (100%), Space: 43 MB (34.04%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,7 +15,8 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
+
+        if(p == null && q == null){ 
             return true;
         }
 
